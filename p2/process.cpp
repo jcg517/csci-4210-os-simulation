3869c8de943a8beba58debaaf29d2e8ade1a26bd
wert18784 <jessegabriel517@gmail.com>
2024-08-15 00:23:37 -0400

    Added process class methods

---
+++ p2/process.cpp
@@ -0,0 +1,33 @@
+#include "process.h"
+
+Process::Process()
+{
+}
+
+
+void Process::update() 
+{
+	this->burst_index++;
+
+	if (onCPUBurst) 
+	{
+		this->prev_t = t;
+		this->prev_tau = tau;
+		this->t = burst_times[burst_index];
+		this->tau = (alpha * prev_t) + (alpha * prev_tau);
+
+	} 
+	else //onIOBurst
+	{
+
+	}
+}
+
+
+void Process::preempt( int elapsed_time )
+ {
+	int time_remaining = burst_times[burst_index] - elapsed_time;
+	burst_times[burst_index] = time_remaining;
+	this->t = time_remaining;
+	this->tau -= elapsed_time ;
+}
--- p2/process.h
+++ p2/process.h
@@ -4,20 +4,34 @@
 class Process {
 private:
   int burst_completion_time = 0;
+  int burst_index = 0;
+  int t = burst_times[0];
+  int tau = 1 / lambda;
+  int prev_t;
+  int prev_tau; 
+
 public:
   char * id;
-  int arrival_time;
   int * burst_times;
-  int cpu_burst_count;
-  int burst_index = 0;
+  int arrival_time;
+  bool is_cpu_bound;
+  int num_cpu_bursts;
+  int num_total_bursts;
+  int curr_cpu_burst_start; 
 
   bool onCPUBurst() { return burst_index % 2 == 0; };
   bool onIOBurst() { return burst_index % 2 != 0; };
-  int currentBurstTime() { return burst_times[burst_index]; };
+
+  int getT() { return t; };
+  int getTau() { return tau; };
+  int getCpuBurstsLeft() { return std::ceil( (num_total_bursts - burst_index ) / 2 ); };
 
   int waitBurst(int current_time) { return burst_completion_time = current_time + burst_times[burst_index]; };
-  int burstCompletionTime() { return burst_completion_time };
+  int burstCompletionTime() { return burst_completion_time; };
+
   void finishBurst() { burst_completion_time = 0; };
+  void update() {}; // Increments burst index
+  void preempt( int elapsed_time ) {};
 };
 
 #endif
